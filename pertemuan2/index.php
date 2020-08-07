4cb036a9426d66b2e6be45b37e0d0cd5c9d5bffd
eyditye <46802253+eyditye@users.noreply.github.com>
2020-08-07 12:24:56 +0700

    Add files via upload
    Basic PHP Syntax

---
+++ pertemuan2/index.php
@@ -0,0 +1,27 @@
+<?php
+// ini adalah komentar
+
+/* */
+	echo "Aditya Ramadhan";
+	var_dump("AKU TAHU");
+?>
+
+<?php 
+	$nama= "Aditya Koykoy";
+	$nami= "Aditya Koykiy";
+	echo $nama . " Apabae " . $nami ;
+	$y= 20;
+	$x= "20";
+	echo $x;
+
+	var_export($y===$x);
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Belajar PHP Sintaks</title>
+</head>
+<body>
+	<h1>Halo, Selamat Datang Aditya</h1>
+</body>
+</html>
